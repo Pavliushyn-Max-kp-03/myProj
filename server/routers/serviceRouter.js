37231774f2090ad56756084be9732c658967c62b
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-03-12 19:19:16 +0200

    make some fixes

--- server/routers/serviceRouter.js
+++ server/routers/serviceRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const serviceController = require('../controllers/serviceController')
+const checkRole = require("../errors/checkRole")
 
-router.post('/', serviceController.create)
+router.post('/', checkRole("ADMIN"), serviceController.create)
 router.get('/', serviceController.getAll)
 router.get('/:id', serviceController.getOne)
 
