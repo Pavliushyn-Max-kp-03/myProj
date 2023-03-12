493f81ee68acd71813b148d18e93df167ebcfff7
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-03-12 16:30:25 +0200

    modifying registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User, Basket} = require('../models/models')
+const generateJWT = (id, email, role) => {
+    return jwt.sign({id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+}
 class UserController{
     async registration(req, res) {
         const {email, password, role} = req.body
@@ -14,7 +17,7 @@ class UserController{
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
         const basket = await Basket.create({userId: user.id})
-        const token = jwt.sign({id: user.id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+        const token = generateJWT(user.id, user.email, user.role)
         return res.json({token})
 
     }
