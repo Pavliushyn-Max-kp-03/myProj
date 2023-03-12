6a0e7d6e5175b71899bc089d68ee974ec341d037
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-03-12 16:22:54 +0200

    made registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,21 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
 class UserController{
     async registration(req, res) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            res.status(500).json({ message: 'Неправильний email або пароль' });
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            res.status(500).json({ message: 'Користувач з вказаним email вже існує' });
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = jwt.sign({id: user.id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+        return res.json({token})
 
     }
     async login(req, res) {
