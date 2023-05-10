6ad85a95595cbbec377342f199492c92cc48ed03
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-05-11 00:13:30 +0300

    begin admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {Container, Button} from "react-bootstrap";
 
 const Admin = () => {
   return (
-    <div>
-      Admin
-    </div>
+    <Container className="d-flex flex-column">
+      <Button
+      variant={"outline-dark"}
+      className="mt-4 p-2">
+        Додати тип
+      </Button>
+      <Button
+      variant={"outline-dark"}
+      className="mt-4 p-2">
+        Додати бренд
+      </Button>
+      <Button
+      variant={"outline-dark"}
+      className="mt-4 p-2">
+        Додати сервіс
+      </Button>
+    </Container>
   );
 }
 
