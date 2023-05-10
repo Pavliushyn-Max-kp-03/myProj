9d08646b2a1f6f57bb780749222a6afc643d1ecf
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-05-10 19:22:04 +0300

    end services page

--- client/src/components/ServiceItem.js
+++ client/src/components/ServiceItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Col, Card} from 'react-bootstrap';
 import Image from "react-bootstrap/Image";
+import { useNavigate } from "react-router-dom"
+import {SERVICE_ROUTE} from "../utils/consts";
+
 
 const ServiceItem = ({service}) => {
+    const navigate = useNavigate()
+    console.log(navigate)
     
     return (
-    <Col md={3} className={"mt-3"}>
+    <Col md={3} className={"mt-3"} onClick={() => navigate(SERVICE_ROUTE + '/' + service.id)}>
         <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
         <Image width={150} height={150} src={service.img}/>
         <div className='text-black-50 mt-1 d-flex justify-content-between align-items-center'>
--- client/src/store/ServiceStore.js
+++ client/src/store/ServiceStore.js
@@ -19,8 +19,8 @@ export default class ServiceStore {
             {id:2, name: "Change battery1", price:21000, img: "https://occ-0-325-1567.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776"},
             {id:3, name: "Change battery", price:21000, img: "https://occ-0-325-1567.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776"},
             {id:4, name: "Change battery", price:21000, img: "https://occ-0-325-1567.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776"},
-            {id:3, name: "Change battery", price:21000, img: "https://occ-0-325-1567.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776"},
-            {id:4, name: "Change battery", price:21000, img: "https://occ-0-325-1567.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776"}
+            {id:5, name: "Change battery", price:21000, img: "https://occ-0-325-1567.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776"},
+            {id:6, name: "Change battery", price:21000, img: "https://occ-0-325-1567.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABbFI2wcwiGkHDdGWaw58hWgLETOBsbqqv6GbKnZFn3s_Y4fjw0Ys9DNYD5txnfV3oj9tgsBeaSnPcBOwQqQnpHVqHeQr9FtvVzaL.jpg?r=776"}
         ]
         this._selectedType = {}
         this._selectedBrand = {}
