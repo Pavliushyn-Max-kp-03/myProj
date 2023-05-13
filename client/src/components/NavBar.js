b29767860a0d2df440bc9729ae1bf97f042a81f5
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-05-13 14:48:41 +0300

    end with registration

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,6 +12,11 @@ import { useNavigate } from "react-router-dom";
 const NavBar = observer(() => {
     const { user } = useContext(Context);
     const navigate = useNavigate();
+
+    const logOut = () => {
+        user.setUser({});
+        user.setIsAuth(false);
+    };
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
@@ -29,7 +34,7 @@ const NavBar = observer(() => {
                         </Button>
                         <Button
                             variant={"outline-light"}
-                            onClick={() => navigate(LOGIN_ROUTE)}
+                            onClick={() => logOut()}
                         >
                             Вихід
                         </Button>
@@ -38,7 +43,7 @@ const NavBar = observer(() => {
                     <Nav className="ml-auto" style={{ color: "white" }}>
                         <Button
                             variant={"outline-light"}
-                            onClick={() => user.setIsAuth(true)}
+                            onClick={() => navigate(LOGIN_ROUTE)}
                         >
                             Авторизація
                         </Button>
