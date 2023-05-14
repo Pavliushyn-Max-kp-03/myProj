e94eb41fdcaeb57092897a86ff52c31476be9409
pl.max03 <pavliushyn.max.kp03@gmail.com>
2023-05-14 20:10:06 +0300

    add creating types from admin panel

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Form } from "react-bootstrap";
+import { createType } from "../../http/serviceApi";
 
 const CreateType = ({ show, onHide }) => {
+    const [value, setValue] = useState("");
+    const addType = () => {
+        createType({ name: value }).then((data) => {
+            setValue("");
+            onHide();
+        });
+    };
     return (
         <Modal show={show} onHide={onHide} size="lg" centered>
             <Modal.Header closeButton>
@@ -13,14 +21,18 @@ const CreateType = ({ show, onHide }) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    <Form.Control placeholder={"Введіть назву типу"} />
+                    <Form.Control
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        placeholder={"Введіть назву типу"}
+                    />
                 </Form>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="outline-danger" onClick={onHide}>
                     Закрити
                 </Button>
-                <Button variant="outline-success" onClick={onHide}>
+                <Button variant="outline-success" onClick={addType}>
                     Додати
                 </Button>
             </Modal.Footer>
